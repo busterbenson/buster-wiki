d420c0534f4791383667404a30a362d9738a2d51
busterbenson <buster@benson.fm>
2019-05-11 22:49:51 -0700

    test netlify functions

--- netlify_functions/algolia.js
+++ netlify_functions/algolia.js
@@ -1,7 +1,6 @@
 exports.handler = function(event, context, callback) {
-  // your server-side functionality
   callback(null, {
-  statusCode: 200,
-  body: "Hello, World"
+    statusCode: 200,
+    body: "Hello, World"
   });
-}
+};
