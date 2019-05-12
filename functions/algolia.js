6cdb02a28eae8be58e93ea88faa6d75859361fd3
busterbenson <buster@benson.fm>
2019-05-11 23:08:26 -0700

    add functions directory

--- functions/algolia.js
+++ functions/algolia.js
@@ -1,7 +1,20 @@
+const algoliasearch = require('algoliasearch');
+
+const algoliaApp = process.env.ALGOLIA_APP_ID;
+const algoliaIndex = process.env.ALGOLIA_INDEX_NAME;
+
 exports.handler = function(event, context, callback) {
 	console.log("Loaded algolia.js");
+
+// Initiate an Algolia client
+  const client = algoliasearch(algoliaApp, context.secrets.ALGOLIA_TOKEN);
+  // Initiate the Algolia index
+  const index = client.initIndex(algoliaIndex);
+  callback(null, 200);
+
+
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: "Done: "+algoliaApp
   });
 };
