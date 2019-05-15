6ee17a27f56d919679857a5e54f1835acb3d2ba1
busterbenson <buster@benson.fm>
2019-05-15 14:18:06 -0700

    fix text overflow on my life by weeks page

--- functions/algolia.js
+++ functions/algolia.js
@@ -6,10 +6,11 @@ const algoliaIndex = process.env.ALGOLIA_INDEX_NAME;
 exports.handler = function(event, context, callback) {
 	console.log("Loaded algolia.js");
 
-// Initiate an Algolia client
+	// Initiate an Algolia client
   const client = algoliasearch(algoliaApp, process.env.ALGOLIA_ADMIN_KEY);
   // Initiate the Algolia index
   const index = client.initIndex(algoliaIndex);
+
   callback(null, {
     statusCode: 200,
     body: "Done: "+algoliaApp+", "+index 
